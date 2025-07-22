1419decadd8e31af9bc8ffafbd482b2317599b3e
slaughtbear <ivanaguirre368@gmail.com>
2025-07-21 20:41:24 -0600

    chore: add cars router

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 from typing import Any
 from fastapi import FastAPI
+from src.routes.cars import cars
 
 
 app = FastAPI()
 
 
+@app.get("/")
 def read_root() -> dict[str, Any]:
     return {'msg': 'welcome to drive and deal backend'}
+
+
+app.include_router(
+    router = cars,
+    prefix = "/api/cars",
+    tags = ["Cars"]
+)
